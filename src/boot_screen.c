2c08a5094dc1b800ff273718332eb543525ffcab
Dan Fields <dan.fields@verizon.net>
2023-01-24 11:12:43 -0500

    O boot screen simulation #26 (fix build)

---
+++ src/boot_screen.c
@@ -0,0 +1,33 @@
+#include "../oserver_gui_demo.h"
+#include "oserver_gui.h"
+#include "menu_handler.h"
+#include "utilities.h"
+
+#include <stdio.h>
+
+extern lv_obj_t * boot_screen;
+
+/*
+ * Display boot screen symbol
+ */
+void display_boot_screen(void)
+{
+    printf("BOOT SCREEN ...\n");
+
+    LV_ASSERT(boot_screen == NULL);
+    boot_screen = lv_img_create(lv_scr_act());  // Assigned to GLOBAL
+
+    lv_obj_set_size(boot_screen, 385, 510); // Same as the simulator dislay
+    lv_img_set_src(boot_screen, &Background);
+
+    // Immediately turn off the page index circles.
+    lv_obj_fade_out(lv_layer_top(), 0, 0);
+    /*
+     * The 'O' metalic symbol
+     */
+    lv_obj_t * boot_screen_symbol = lv_img_create(boot_screen);
+    lv_img_set_src(boot_screen_symbol, &Logo);
+    lv_obj_align(boot_screen_symbol, LV_ALIGN_TOP_MID, 0, 190);
+
+    lv_obj_fade_in(boot_screen, 1000, 3000);
+}
