bfc0d95a0d62e92cbe0f8717ff831f02b2b7cb80
Snowlark <steve@snowlark.com>
2024-05-13 14:01:32 -0700

    Update contacts.c
    adding initialization chunk

--- src/contacts.c
+++ src/contacts.c
@@ -108,21 +108,21 @@ static contacts_item contacts_list[CONTACT_LIST_MAX] = {
 /* Set variables to calculate and then truncate strings too wide for the viewport -- insert an ellipsis in place of the long string */
 static const char * from_string;
 static int from_count;
-// const char * summary_string;
-int summary_count;
 static const char * notes_string;
 static int notes_count;
+// const char * summary_string;
+// int summary_count;
 
 /* Set variables for all the message content fields: ID, FROM, SUBJECT, MESSAGE */
 lv_label_t * ctcitem_id;
 lv_label_t * ctcitem_name;
 lv_label_t * ctcitem_notes;
 lv_label_t * ctcitem_status;
-// lv_label_t * txtmsg_message;
 lv_label_t * contact_detail_from;
-// lv_obj_t * text_detail_message;
 static lv_obj_t * top_of_list_items;
 static lv_obj_t * spacer;
+// lv_label_t * txtmsg_message;
+// lv_obj_t * text_detail_message;
 
 /* The following function populates the main screen with read and unread text messages */
 void contacts_list_init(lv_obj_t * ctcitem_page) {
@@ -266,11 +266,32 @@ void contacts_view(lv_obj_t * contacts_page) {
 
 void contacts_menu_setup(void)
 {
+
+    printf("CONTACTS MENU init...\n");
+
+    lv_obj_t * contacts_page = lv_obj_create(NULL);
+    menu_dispatch_table[CONTACTS_VEC] = contacts_page;
+
+    lv_obj_center(contacts_page);
+    lv_obj_set_style_bg_color(contacts_page, lv_color_lighten(lv_color_black(), 60), 0);
+    lv_obj_set_flex_flow(contacts_page, LV_FLEX_FLOW_ROW);
+    //lv_obj_add_event_cb(contacts_page, scroll_event_cb, LV_EVENT_SCROLL, NULL);
+    lv_obj_set_style_clip_corner(contacts_page, true, 3);
+    lv_obj_set_scroll_dir(contacts_page, LV_DIR_HOR);
+    lv_obj_set_scroll_snap_x(contacts_page, LV_SCROLL_SNAP_CENTER);
+    lv_obj_set_scrollbar_mode(contacts_page, LV_OBJ_FLAG_SCROLL_ONE | LV_SCROLLBAR_MODE_OFF);
+
+    /* MAIN-SCREEN: Display the list of text messages: unread and read comingled together */
+    txtmsg_list_init(contacts_page);
+
+    /* MESSAGE VIEW: Display the text message FROM and MESSAGE */
+    text_message_view(contacts_page);
+
+/*
     lv_obj_t * contacts_page = lv_obj_create(NULL);
     lv_obj_t * background = lv_img_create(contacts_page);
     lv_img_set_src(background, &Background);
 
-    printf("CONTACTS MENU init...\n");
 
     menu_dispatch_table[CONTACTS_VEC] = contacts_page;
 
@@ -283,4 +304,6 @@ void contacts_menu_setup(void)
     lv_obj_set_scroll_dir(contacts_page, LV_DIR_HOR);
     lv_obj_set_scroll_snap_x(contacts_page, LV_SCROLL_SNAP_CENTER);
     lv_obj_set_scrollbar_mode(contacts_page, LV_OBJ_FLAG_SCROLL_ONE | LV_SCROLLBAR_MODE_OFF);
+*/
+
 }
