eba868be8a82e0eb8f0d01b66b7bee021e8e3c15
Snowlark <steve@snowlark.com>
2024-05-14 14:24:34 -0700

    Update contacts.c
    adding 10 more contacts for a total of 20

--- src/contacts.c
+++ src/contacts.c
@@ -183,6 +183,106 @@ static contacts_item contacts_list[] = {
         .contact_notes = "Organization Name, Blue Blaze Irregulars, Organization Title, Team Member, Organization Department, "
     },
     {
+        .contact_id = "0011",
+        .contact_name = "Pinky Carruthers",
+        .contact_fname = "Pinky",
+        .contact_lname = "Carruthers",
+        .contact_p_email = "Pinky+Carruthers@gmail.com",
+        .contact_m_phone = "650.555.0210",
+        .contact_w_address = "2010 El Camino Real, San Mateo, CA 94403",
+        .contact_notes = "Organization Name, Blue Blaze Irregulars, Organization Title, Team Member, Organization Department, "
+    },
+    {
+        .contact_id = "0012",
+        .contact_name = "Billy Travers",
+        .contact_fname = "Billy",
+        .contact_lname = "Travers",
+        .contact_p_email = "Billy+Travers@gmail.com",
+        .contact_m_phone = "650.555.0211",
+        .contact_w_address = "2010 El Camino Real, San Mateo, CA 94403",
+        .contact_notes = "Organization Name, Blue Blaze Irregulars, Organization Title, Team Member, Organization Department, "
+    },
+    {
+        .contact_id = "0013",
+        .contact_name = "Dr Masado Banzai",
+        .contact_fname = "Masado",
+        .contact_lname = "Banzai",
+        .contact_p_email = "Masado+Banzai@gmail.com",
+        .contact_m_phone = "650.555.0212",
+        .contact_w_address = "500 W University Ave, El Paso, TX 79968",
+        .contact_notes = "Organization Name, UT El Paso, Organization Title, Scientist, Organization Department, "
+    },
+    {
+        .contact_id = "0014",
+        .contact_name = "Dr Sandra Banzai",
+        .contact_fname = "Sandra",
+        .contact_lname = "Banzai",
+        .contact_p_email = "Sandra+Banzai@gmail.com",
+        .contact_m_phone = "650.555.0213",
+        .contact_w_address = "500 W University Ave, El Paso, TX 79968",
+        .contact_notes = "Organization Name, UT El Paso, Organization Title, Scientist, Organization Department, "
+    },
+    {
+        .contact_id = "0015",
+        .contact_name = "Lord John Whorfin",
+        .contact_fname = "John",
+        .contact_lname = "Whorfin",
+        .contact_p_email = "John+Whorfin@gmail.com",
+        .contact_m_phone = "650.555.1212",
+        .contact_w_address = "8900 De Soto Ave, Canoga Park, CA 91304",
+        .contact_notes = "Organization Name, Yoyodyne Propulsion Systems, Organization Title, , Organization Department, "
+    },
+    {
+        .contact_id = "0016",
+        .contact_name = "John Parker",
+        .contact_fname = "John",
+        .contact_lname = "Parker",
+        .contact_p_email = "John+Parker@gmail.com",
+        .contact_m_phone = "650.555.0208",
+        .contact_w_address = "Planet 10, Redwood City, CA 94063",
+        .contact_notes = "Organization Name, Planet 10, Organization Title, Pilot, Organization Department, Lectroid Flight Team"
+    },
+    {
+        .contact_id = "0017",
+        .contact_name = "John O'Connor",
+        .contact_fname = "John",
+        .contact_lname = "O'Connor",
+        .contact_p_email = "John+O'Connor@gmail.com",
+        .contact_m_phone = "650.555.1212",
+        .contact_w_address = "8900 De Soto Ave, Canoga Park, CA 91304",
+        .contact_notes = "Organization Name, Yoyodyne Propulsion Systems, Organization Title, Lord, Organization Department, Lectroid Commander"
+    },
+    {
+        .contact_id = "0018",
+        .contact_name = "John Emdall",
+        .contact_fname = "John",
+        .contact_lname = "Emdall",
+        .contact_p_email = "John+Emdall@gmail.com",
+        .contact_m_phone = "650.555.0208",
+        .contact_w_address = "Planet 10, Redwood City, CA 94063",
+        .contact_notes = "Organization Name, Planet 10, Organization Title, Chief, Organization Department, Black Lectroids"
+    },
+    {
+        .contact_id = "0019",
+        .contact_name = "John Bigboote",
+        .contact_fname = "John",
+        .contact_lname = "Bigboote",
+        .contact_p_email = "John+Bigboote@gmail.com",
+        .contact_m_phone = "650.555.1212",
+        .contact_w_address = "8900 De Soto Ave, Canoga Park, CA 91304",
+        .contact_notes = "Organization Name, Yoyodyne Propulsion Systems, Organization Title, , Organization Department, "
+    },
+    {
+        .contact_id = "0020",
+        .contact_name = "John Wright",
+        .contact_fname = "John",
+        .contact_lname = "Wright",
+        .contact_p_email = "John+Wright@gmail.com",
+        .contact_m_phone = "650.555.1212",
+        .contact_w_address = "8900 De Soto Ave, Canoga Park, CA 91304",
+        .contact_notes = "Organization Name, Yoyodyne Propulsion Systems, Organization Title, , Organization Department, "
+    },
+    {
         .contact_id = "end",
         .contact_notes = "TAP HERE\nTo import\nyour Contacts",
     },
