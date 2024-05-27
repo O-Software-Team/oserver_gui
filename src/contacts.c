950ccceba10f92de9f3d40ca565c41fd87e2ae86
Snowlark <steve@snowlark.com>
2024-05-27 01:33:26 -0700

    Update contacts.c
    .

--- src/contacts.c
+++ src/contacts.c
@@ -481,7 +481,12 @@ void contacts_view(lv_obj_t * contacts_view_page) {
     contact_initials_text = lv_img_create(image);
     fname_1st_char = contacts_list[CONTACT_ID].contact_fname;
     lname_1st_char = contacts_list[CONTACT_ID].contact_lname;
-    contact_initials_set = fname_1st_char + lname_1st_char;
+
+    std::string s;
+    s += fname_1st_char;
+    s += lname_1st_char;
+    contact_initials_set = s;
+    // contact_initials_set = fname_1st_char + lname_1st_char;
 
     printf("\nFname char: %c   Lname char: %c  \n", fname_1st_char[0], lname_1st_char[0]);
     printf("CTC Inits: %s \n", contact_initials_set);
