7096e4471c5189d447897bf0201ddbc6c62fd007
Snowlark <steve@snowlark.com>
2024-06-06 13:08:10 -0700

    Update files.c
    .

--- src/files.c
+++ src/files.c
@@ -485,7 +485,7 @@ void filesystem_04_view(lv_obj_t * filesystem_04_view_page) {
     lv_label_t * list_name = lv_label_create(image);
     lv_label_set_recolor(list_name, true);
     lv_obj_align(list_name, LV_ALIGN_TOP_LEFT, CONTACT_PAD_LEFT, 112);
-    lv_label_set_text(list_name, "Your movies");
+    lv_label_set_text(list_name, "Your images");
     lv_obj_set_style_text_color(list_name, lv_color_hex(CONTACT_SUBDUED_COLOR), 0);
     lv_obj_set_style_text_font(list_name, &NeueHaasDisplayLight_24, LV_PART_MAIN);
 
@@ -517,11 +517,11 @@ void filesystem_04_view(lv_obj_t * filesystem_04_view_page) {
 
     file_icon[f] = lv_img_create(image);
     lv_img_set_src(file_icon[f], filesystem_04_list[f].file_icon);
-    lv_obj_align(file_icon[f], LV_ALIGN_CENTER, -130, offset - 209);
+    lv_obj_align(file_icon[f], LV_ALIGN_LEFT_MID, 25, offset - 209);
 
     file_label[f] = lv_label_create(image);
     lv_label_set_recolor(file_label[f], true);
-    lv_obj_align(file_label[f], LV_ALIGN_LEFT_MID, 125, offset - 209);
+    lv_obj_align(file_label[f], LV_ALIGN_LEFT_MID, 60, offset - 209);
     lv_label_set_text(file_label[f], filesystem_04_list[f].file_fullname);
     lv_style_set_text_font(&name_style, &NeueHaasDisplayLight_24);
     lv_obj_add_style(file_label[f], &name_style, LV_PART_MAIN);
@@ -529,7 +529,7 @@ void filesystem_04_view(lv_obj_t * filesystem_04_view_page) {
 
     next_icon[f] = lv_img_create(image);
     lv_img_set_src(next_icon[f], &Icon_Next_White);
-    lv_obj_align(next_icon[f], LV_ALIGN_CENTER, 130, offset - 209);
+    lv_obj_align(next_icon[f], LV_ALIGN_LEFT_MID, 135, offset - 209);
     lv_obj_set_style_opa(next_icon[f], LV_OPA_70, LV_PART_MAIN);
 
     list_item_separator[f] = lv_img_create(image);
