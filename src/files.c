9890d079e75f2919aeffa4c38b203953166585b5
Snowlark <steve@snowlark.com>
2024-06-07 09:38:46 -0700

    Update files.c
    .

--- src/files.c
+++ src/files.c
@@ -617,11 +617,11 @@ void filesystem_04_view(lv_obj_t * filesystem_04_view_page) {
         lv_obj_align(file_label[image_record], LV_ALIGN_LEFT_MID, 78, offset - 199);
 
         /* Calculate and then truncate if the NAME field is greater than or equal to 25 characters; then insert an ellipsis in place of the long string */
-        if(fs_04_fullname_count >= 17) {
+        if(fs_04_fullname_count >= 19) {
             lv_label_set_text(file_label[image_record], filesystem_04_list[image_record].file_fullname);
             // lv_label_set_text(contact_name, contacts_01_list[image_record].contact_name);
-            lv_label_cut_text(file_label[image_record],15,fs_04_fullname_count);
-            lv_label_ins_text(file_label[image_record],17,"...");
+            lv_label_cut_text(file_label[image_record],17,fs_04_fullname_count);
+            lv_label_ins_text(file_label[image_record],19,"...");
         } else {
             lv_label_set_text(file_label[image_record], filesystem_04_list[image_record].file_fullname);
             // lv_label_set_text(contact_name, contacts_01_list[image_record].contact_name);
